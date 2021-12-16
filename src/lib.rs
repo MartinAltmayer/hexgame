1a9a7c2655510468eda97b988ec6db4b14eef8a0
Martin Altmayer <altmayer@posteo.de>
2021-12-16 16:03:04 +0100

    Export InvalidMove

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ mod game;
 mod square_array;
 mod union_find;
 
-pub use crate::board::{Board, Color, MAX_BOARD_SIZE, MIN_BOARD_SIZE};
+pub use crate::board::{Board, Color, InvalidMove, MAX_BOARD_SIZE, MIN_BOARD_SIZE};
 pub use crate::coords::Coords;
 pub use crate::game::{Game, Status};
